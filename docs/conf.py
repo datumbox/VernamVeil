6ac12754385d87efd48be00f05bfc47e849451df
Vasilis Vryniotis <bbriniotis@datumbox.com>
2025-05-08 22:09:28 +0100

    Adding logo and favicon in the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,11 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 
+html_logo = "logo.png"
+html_favicon = "favicon.ico"
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+
 autodoc_inherit_docstrings = True
 autoclass_content = "both"
 
