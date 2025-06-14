aed5f86be53fb8c8f62b21b606dd8fb6e00b09bd
Vasilis Vryniotis <bbriniotis@datumbox.com>
2025-06-14 22:49:18 +0100

    Second workaround for conditionally cffi modules

---
+++ nphash_c_ext_trigger/__init__.py
@@ -0,0 +1,1 @@
+# This is a dummy package used as a flag for setup.py to determine if C extensions should be built.
--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 import os
-
 from setuptools import setup
 
-os.environ["SETUPTOOLS_BUILD"] = "1"
+try:
+    # Attempt to import the dummy package to check if C extensions should be built
+    import nphash_c_ext_trigger
+    c_ext = True
+except ImportError:
+    c_ext = False
 
-setup(
-    cffi_modules=[
+if c_ext:
+    os.environ["SETUPTOOLS_BUILD"] = "1"
+    cffi_modules_list = [
         "nphash/_build_utils/_ffi_builders.py:_get_bytesearch_ffi",
         "nphash/_build_utils/_ffi_builders.py:_get_npblake2b_ffi",
         "nphash/_build_utils/_ffi_builders.py:_get_npblake3_ffi",
         "nphash/_build_utils/_ffi_builders.py:_get_npsha256_ffi",
-    ],
-    zip_safe=False,
-)
+    ]
+else:
+    cffi_modules_list = []
+    if "SETUPTOOLS_BUILD" in os.environ:
+        del os.environ["SETUPTOOLS_BUILD"]
+
+setup(cffi_modules=cffi_modules_list, zip_safe=False)
